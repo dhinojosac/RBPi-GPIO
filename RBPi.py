9e36614f95266ac41503be3313e668cc38f3882f
dhinojosac <d.hinojosac.cordova@gmail.com>
2016-08-18 22:16:59 -0300

    Se ordena y agrega version

--- RBPi.py
+++ RBPi.py
@@ -1,3 +1,13 @@
+#RBPi.py
+#
+# Raspberry Pi B 2 - Gpio 
+#
+# 2016 Diego O. Hinojosa Cordova
+# d.hinojosa.cordova@gmail.com
+#
+
+__version__ = 'v0.0.1'
+
 import platform
 import os.path
 import time
@@ -9,6 +19,10 @@ else:
     legacy_id = False
 
 print "legacy_id: {}".format(legacy_id)
+
+# Ger version 
+def getVersion():
+    return __version__
 
 # Constans
 OUT = "out"
@@ -33,38 +47,38 @@ def export(kernel_id):
         f = open('/sys/class/gpio/export','w')
         if legacy_id:
             f.write(str(kernel_id))
-	    print "f_export: {}".format(f)
+        print "f_export: {}".format(f)
         else:
             print "Error no Linux"
-	    pass
-	f.close()
+        pass
+    f.close()
 
 
 # Define direction INPUT or OUTPUT
 def direction(kernel_id, direction):
     iopath = get_gpio_path(kernel_id)
     if os.path.exists(iopath):
-	f = open(iopath + '/direction','w')
-	f.write(direction)
-	print "f: {}".format(f)
-	f.close
+    f = open(iopath + '/direction','w')
+    f.write(direction)
+    print "f: {}".format(f)
+    f.close
 
 # Set value to pin
 def output(pin, value):
     iopath = get_gpio_path(pin)
     if os.path.exists(iopath):
         f = open(iopath + '/value','w')
-	f.write(str(value))
-	f.close
+    f.write(str(value))
+    f.close
 
 # Read pin value
 def input(pin):
     iopath = get_gpio_path(pin)
     if os.path.exists(iopath):
         f = open(iopath + '/value','r')
-	a = f.read()
-	f.close()
-	return  int(a)
+    a = f.read()
+    f.close()
+    return  int(a)
 
 # Get path for manipulate pins
 def get_gpio_path(kernel_id):
@@ -74,8 +88,8 @@ def get_gpio_path(kernel_id):
     if legacy_id:
         iopath="/sys/class/gpio/gpio%d" % (kernel_id)
     elif not legacy_id:
-	print "No Raspberry Pi (platform() not Linux)"
-	return "ERROR"
+    print "No Raspberry Pi (platform() not Linux)"
+    return "ERROR"
         sys.exit()
     print "iopath: {}".format(iopath)
     return  iopath
